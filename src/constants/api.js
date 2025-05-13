1185a904fbad3fc3ddb8950ea28e71c3f9e4b608
seunggonhyeon <hyunsg5638@gmail.com>
2025-05-13 17:59:10 +0900

    feat:ENDPOINT 관리 파일 생성

---
+++ src/constants/api.js
@@ -0,0 +1,9 @@
+// src/contants/api.js
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// ENDPOINTS.egen("서울특별시","강남구")
+
+export const ENDPOINTS = {
+  egen: (stage1, stage2) =>
+    `${API_BASE_URL}/egen?stage1=${stage1}&stage2=${stage2}`,
+};
